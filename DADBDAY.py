8c6f2c49cc5bba7f32ebd51a7f5ca9768f3c17f1
Nelluri-Yoffi <nelluriyoffi@gmail.com>
2026-09-04 23:28:04 +0530

    dadBday

---
+++ DADBDAY.py
@@ -0,0 +1,20 @@
+import time
+
+print("Get ready...")
+time.sleep(1)
+
+for i in range(3, 0, -1):
+    print(i, "...")
+    time.sleep(1)
+
+print("\n🎉 HAPPY BIRTHDAY DAD! 🎉\n")
+
+balloons = """
+   (   )   (   )
+    )_(     )_(
+   (   )   (   )
+"""
+print(balloons)
+
+note = "Thank you for being the best dad ever, I love you sm, HAPPY BIRTHDAY OLDIEEEE"
+print(note)
