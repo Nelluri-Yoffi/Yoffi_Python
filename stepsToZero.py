3c7d88b75b9b86c6dcfc78b5f3e8d2069d3e0fe7
Nelluri-Yoffi <nelluriyoffi@gmail.com>
2026-09-13 22:32:06 +0530

    stepsToZero

---
+++ stepsToZero.py
@@ -0,0 +1,10 @@
+def numberOfSteps(num):
+    steps = 0
+    while num != 0:
+        if num % 2 == 0:
+            num = num // 2
+        else:
+            num = num - 1
+        steps = steps + 1
+    return steps
+print(numberOfSteps(14))
